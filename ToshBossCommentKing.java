6b5e1a203781798b1fad5b9da7df6d0bc8336b86
ToshBoss <mailtojoshuaphilip@gmail.com>
2020-02-21 09:38:49 -0500

    Update ToshBossCommentKing.java

--- ToshBossCommentKing.java
+++ ToshBossCommentKing.java
@@ -6,7 +6,7 @@
 import java.util.*;
 
                                                                                                    
-public class CommentKing
+public class ToshBossCommentKing
 {
    public static void main (String args[])
    {
