ebf2ef0a3bbc8c9ea54979b69d3c6ec7040c3bf4
ToshBoss <mailtojoshuaphilip@gmail.com>
2020-02-21 10:57:17 -0500

    Update ToshBossCommentKing.java

--- ToshBossCommentKing.java
+++ ToshBossCommentKing.java
@@ -13,7 +13,14 @@ public class ToshBossCommentKing
       //make console
       Scanner console  = new Scanner(System.in);
       
-      System.out.println("Enter a phrase: ");
+      System.out.println("___________          .__   __________                     ");
+      System.out.println("\\__    ___/___  _____|  |__\\______   \\ ____  ______ ______");
+      System.out.println("  l    l /  _ \\/  ___/  l  \\l    l  _//  _ \\/  ___//  ___/");
+      System.out.println("  l    l(  <_> )___ \\ l   Y  \\   l    (  <_> )___ \\ \\___ \\ ");
+      System.out.println("  l____l \\____//____  >___l  /______  /\\____/____  >____  >");
+      System.out.println("                   \\/     \\/       \\/           \\/     \\/ ");
+            System.out.println("Enter a phrase: ");
+
       String phrase = console.nextLine();
       System.out.println();
       
@@ -21,7 +28,7 @@ public class ToshBossCommentKing
       int count = 1;
       for(int i = 0; i < phrase.length(); i++){
       output+= phrase.charAt(i);
-      if(count >= 40 && phrase.charAt(i) == ' '){
+      if(count >= 60 && phrase.charAt(i) == ' '){
          count = 0;
          output+="\n * ";
       }
@@ -34,4 +41,3 @@ public class ToshBossCommentKing
 
 
 }
-
